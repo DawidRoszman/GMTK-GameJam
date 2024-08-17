51ae16d82e0c68d22bf2e3306d98b01706424c85
Dawid Roszman <dawidroszman@protonmail.com>
2024-08-17 18:58:52 +0100

    Revert "Merge pull request #1 from DawidRoszman/shooting"
    This reverts commit 16e9690b67f0ef74ece117b3553ba2f9183a78f8, reversing
    changes made to d68fe7d30f143ca929e25b7a6cf5be4b7f83b647.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,36 +1,18 @@
-class_name Player
 extends CharacterBody2D
 
-
 const SPEED = 800
-const JUMP_POWER = -2500
-
-@export var max_rotation = -160
-@export var min_rotation = -240
+const JUMP_POWER = -1800
 
 const ACCELERATION = 2500
 const FRICTION = 2300
 
-const GRAVITY = 8000
+const GRAVITY = 4000
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	var input_dir = input()
 	if input_dir != Vector2.ZERO:
-		if input_dir == Vector2.RIGHT:
-			#max_rotation = 45
-			#min_rotation = -45
-			#$WandPivot/ShootingPoint.position = Vector2(147, -115)
-			#$WandPivot/Wand.flip_v = false
-			$Character.flip_h = false
-		if input_dir == Vector2.LEFT:
-			#max_rotation = -140
-			#min_rotation = -230
-			#$WandPivot/ShootingPoint.position = Vector2(120, 45)
-			#$WandPivot/Wand.flip_v = true
-			$Character.flip_h = true
-			
 		accelerate(input_dir, delta)
 	else:
 		add_friction(delta)
@@ -38,7 +20,7 @@ func _physics_process(delta: float) -> void:
 	jump()
 func input() -> Vector2:
 	var input_dir = Vector2. ZERO
-	input_dir.x = Input.get_axis("move_left", "move_right")
+	input_dir.x = Input. get_axis("move_left", "move_right")
 	input_dir = input_dir.normalized()
 	return input_dir
 func accelerate(direction, delta):
@@ -51,9 +33,4 @@ func jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_POWER
 	if Input.is_action_just_released("jump") and velocity.y < 0:
-		velocity.y  = JUMP_POWER / 16
-
-func get_min_rotation():
-	return min_rotation
-func get_max_rotation():
-	return max_rotation
+		velocity.y  = JUMP_POWER / 8
--- scripts/shooting_point.gd
+++
@@ -1,8 +0,0 @@
-extends Marker2D
-@onready var player: Player = $"../.."
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-
-	look_at(get_global_mouse_position())
-	#rotation_degrees = clamp(rotation_degrees, -45, 45)
