90ece223017c4ee6856d0cf33a1e3b28bb5b818c
Dawid Roszman <dawidroszman@protonmail.com>
2024-08-18 09:43:08 +0100

    added following player to enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,8 +1,14 @@
 class_name Enemy
 extends RigidBody2D
 
+const speed = 5
+var is_chasing_player = false
+var player = null
+
 func _physics_process(delta: float) -> void:
-	pass
+	if not is_chasing_player:
+		return
+	position += (player.position - position)/speed * delta
 
 func increase(multiplier: Vector2) -> void:
 	if scale > Vector2(2,2):
@@ -14,3 +20,12 @@ func decrease(multiplier: Vector2) -> void:
 		queue_free()
 		return
 	scale -= multiplier
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player = body
+	is_chasing_player = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	is_chasing_player = false
